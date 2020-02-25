44cacbfa28834ad97815c486e194aa03ddd054c0
Tianhui Michael Li <tianhuil@cs.princeton.edu>
2020-02-25 15:00:11 -0800

    mui css

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,25 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Button from 'muicss/lib/react/button'
 import { add } from '@vbm/common'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React {add(2, 3)}
-        </a>
-      </header>
+      <Button>Test Button</Button>
+      <div>
+      Learn React {add(2, 3)}
+      </div>
     </div>
   );
 }
