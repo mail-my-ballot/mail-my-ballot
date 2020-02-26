bfb6b3288cfc0c949b1ba587bbf9ff26fdd1fb18
Tianhui Michael Li <tianhuil@cs.princeton.edu>
2020-02-25 17:01:53 -0800

    Proper async function handling

--- packages/client/src/components/Form.tsx
+++ packages/client/src/components/Form.tsx
@@ -13,25 +13,20 @@ export const InitialForm: React.StatelessComponent = () => {
   let ref: any  // needs to be both `Input | null` and have undeclared value controlEl
   const [displayAddr, setAddr] = React.useState('')
 
-  function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+  async function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+    event.persist()  // allow async function call
+    event.preventDefault()
+
     const inputAddr = ref.controlEl.value
     console.log(inputAddr)
-    fetch(`https://nominatim.openstreetmap.org/search/${inputAddr}?format=json&countrycodes=us`)
-      .then(
-        res => res.json()
-      ).then(
-        json => {
-          console.log(json[0].display_name)
-          setAddr(json[0].display_name)
-        }
-      )
-
-    event.preventDefault()
+    const result = await fetch(`https://nominatim.openstreetmap.org/search/${inputAddr}?format=json&countrycodes=us`)
+    const json = await result.json()
+    setAddr(json[0].display_name)
   }
 
   return <Form onSubmit={handleSubmit}>
     <legend>Address</legend>
-    <Input label='Address' floatingLabel={true} ref={el => ref = el} ></Input>
+    <Input label='Address' floatingLabel={true} ref={el => ref = el} defaultValue='230 ashland pl, brooklyn ny'></Input>
     {displayAddr ? <p>{displayAddr}</p> : null}
     <SubmitButton color='primary' variant='raised'>Submit</SubmitButton>
   </Form>
