80536f3e29716c1cecd508fd393c4dff97b3725e
Tianhui Michael Li <tianhuil@cs.princeton.edu>
2020-02-25 16:32:06 -0800

    Query to get location

--- packages/client/src/components/Form.tsx
+++ packages/client/src/components/Form.tsx
@@ -10,17 +10,24 @@ const SubmitButton = styled(Button)`
 `
 
 export const InitialForm: React.StatelessComponent = () => {
-  const zip = React.useRef()
-  const addr = React.useRef()
+  const addr = React.useRef<Input>(null)
 
   function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+    const addrStr = '230 ashland pl, brooklyn ny'
 
+    fetch(`https://nominatim.openstreetmap.org/search/${addrStr}?format=json&countrycodes=us`)
+      .then(
+        res => res.json()
+      ).then(
+        json => console.log(json)
+      )
+
+    event.preventDefault()
   }
 
   return <Form onSubmit={handleSubmit}>
     <legend>Address</legend>
-    <Input label='Zipcode' floatingLabel={true}></Input>
-    <Input label='Address' floatingLabel={true}></Input>
+    <Input label='Address' floatingLabel={true} ref={addr} defaultValue='230 ashland pl, brooklyn ny'></Input>
     <SubmitButton color='primary' variant='raised'>Submit</SubmitButton>
   </Form>
 }
