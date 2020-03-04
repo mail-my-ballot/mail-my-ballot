4a30b99a308fdde169de077b8c2fd333f61da183
Tianhui Michael Li <tianhuil@cs.princeton.edu>
2020-03-04 15:47:40 -0800

    Remove FL from main page

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -4,7 +4,6 @@ import styled from 'styled-components'
 
 import { client } from './lib/trpc'
 import { InitialForm } from './comp/Form'
-import { FL } from './comp/states/FL'
 import { QueryContainer, LocaleContainer } from './lib/state'
 
 const AppContainer = styled(Container)`
@@ -29,7 +28,6 @@ function App() {
       <LocaleContainer.Provider>
         <AppContainer>
           <InitialForm/>
-          <FL/>
           <div>
             Learn React {sum}
           </div>
