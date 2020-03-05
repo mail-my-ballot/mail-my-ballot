371fd5da25f2548b5f11b4bba59a11691b1a6ed3
Tianhui Michael Li <tianhuil@cs.princeton.edu>
2020-03-05 15:09:14 -0800

    rename app

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -9,26 +9,26 @@ import { processEnvOrThrow } from './common'
 import { VbmRpc } from './service/trpc';
 import { registerExpressHandler } from '@tianhuil/simple-trpc/dist/handler/express'
 
-const App = Express();
+const app = Express();
 
 // logging middleware
-App.use(Morgan('combined'));
+app.use(Morgan('combined'));
 
-App.use(cors())
+app.use(cors())
 
-App.get('/', (req: Request, res: Response) => {
+app.get('/', (req: Request, res: Response) => {
   res.send('Hello World');
 });
 
-registerExpressHandler(App, new VbmRpc())
+registerExpressHandler(app, new VbmRpc())
 
 // https://github.com/GoogleCloudPlatform/nodejs-getting-started/tree/master/1-hello-world
 if (module === require.main) {
   const port = parseInt(processEnvOrThrow('SERVER_PORT'))
-  const server = App.listen(port, () => {
+  const server = app.listen(port, () => {
     const port = (<AddressInfo>server.address()).port;
-    console.log(`App listening on port ${port}`);
+    console.log(`app listening on port ${port}`);
   });
 }
 
-export default App;
+export default app;
