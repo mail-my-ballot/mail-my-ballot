ce573d77c7677f8ffb1b8cc2d471b52f83df8804
Tianhui Michael Li <tianhuil@cs.princeton.edu>
2020-03-10 14:55:59 -0700

    first attempt

--- packages/client/src/lib/trpc.ts
+++ packages/client/src/lib/trpc.ts
@@ -2,6 +2,9 @@ import { makeClient, httpConnector } from '@tianhuil/simple-trpc/dist/client'
 import { IVbmRpc, processEnvOrThrow } from '../common'
 
 const serverUrl = processEnvOrThrow('REACT_APP_SERVER')
+const timeout = parseInt(processEnvOrThrow('REACT_APP_TIMEOUT'))
+console.log(timeout)
+
 export const client = makeClient<IVbmRpc>(
-  httpConnector(serverUrl, {timeout: 5000})
+  httpConnector(serverUrl, {timeout, requestInit: {mode: 'no-cors'}})
 )
