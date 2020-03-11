955a17eec05d01588429f33b38cb11f485d9c02f
Tianhui Michael Li <tianhuil@cs.princeton.edu>
2020-03-11 13:07:37 -0700

    Update message

--- packages/client/src/comp/WarningMsg.tsx
+++ packages/client/src/comp/WarningMsg.tsx
@@ -4,7 +4,10 @@ import { RedOutline } from './util/RedOutline'
 const RawWarningMsg = () => {
   return (<RedOutline>
     <h3>Warning: Not Production!</h3>
-    <p>This is not a production build.  No emails will be sent to county officials.  You can safely play with this demo.  This message will not appear in production.
+    <p>This is not a production build.
+      If this were production, the registration email would be sent to both the county official and yourself.
+      Since this is not production, the email will only be sent to you.
+      No email will be sent to a county official so you can safely play with this demo.
     </p>
   </RedOutline>)
 }
