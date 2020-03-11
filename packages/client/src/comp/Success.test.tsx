470c2d7344ddee437b735f7a6793e3f1ab03a964
Tianhui Michael Li <tianhuil@cs.princeton.edu>
2020-03-11 16:19:40 -0700

    Tes Success.tsx

--- packages/client/src/comp/Success.test.tsx
+++ packages/client/src/comp/Success.test.tsx
@@ -1,24 +1,14 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import { Success } from './Success';
-import { MemoryRouter, Route } from 'react-router-dom';
-import { MemoryRouterProps } from 'react-router'
-
-const Router: React.FC<MemoryRouterProps> = (props) => (
-  <MemoryRouter {...props} initialEntries={['/success#hash']} initialIndex={0}/>
-)
+import { MemoryRouter } from 'react-router-dom';
 
 test('renders learn react link', () => {
   const { getByText } = render(
-    <Route path='/success'>
-      <Success/>
-    </Route>, {
-    wrapper: Router
+    <Success/>, {
+    wrapper: MemoryRouter
   })
   const h1 = getByText(/^Congratulations!$/i)
   expect(h1).toBeInTheDocument()
 
-  // const confCode = getByText(/hash/i)
-  // expect(confCode).toBeInTheDocument()
 });
-//
