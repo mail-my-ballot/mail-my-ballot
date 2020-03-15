3ad939f8067cf6848e8c4cde7a2e36dccd6b5dad
Tianhui Michael Li <tianhuil@cs.princeton.edu>
2020-03-15 14:26:50 -0700

    Add michigan search capability

--- packages/server/src/service/contact/index.ts
+++ packages/server/src/service/contact/index.ts
@@ -1,13 +1,11 @@
 import { floridaContacts } from './florida'
-import { michiganContacts } from './michigan'
+import * as michigan from './michigan'
 import { Contact, State, Locale, FloridaContact } from '../../common'
 
 export const getContact = <S extends State>({state, county, city}: Locale<S>): Contact | null => {
-  const addState = <T>(state: S, obj: T): T & {state: S} => ({state, ...obj})
-
   switch(state) {
     case 'Florida': return {state: 'Florida', ...floridaContacts[county]}
-    case 'Michigan': return {state: 'Michigan', ...michiganContacts[county + ':' + city]}
+    case 'Michigan': return michigan.search(county, city)
     default: return null
   }
 }
