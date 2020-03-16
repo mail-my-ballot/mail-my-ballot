b2e733c1170bbb35d6fa696ffbd01bddf155ec5d
Tianhui Michael Li <tianhuil@cs.princeton.edu>
2020-03-16 11:43:22 -0700

    Fix osm test

--- packages/client/src/lib/osm.test.ts
+++ packages/client/src/lib/osm.test.ts
@@ -1,23 +1,28 @@
 import { geocode } from './osm'
 
-test('OSM is returning stable results', async () => {
-  const testCases: [string, string][] = [
-    ['100 S Biscayne Blvd, Miami, FL 33131', 'Miami-Dade County, Florida'],
-    ['5443 Main St, Port Richey, FL 34652', 'Pasco County, Florida'],
-    ['2000 W Commercial Blvd, Fort Lauderdale, FL 33309', 'Broward County, Florida'],
-    ['45525 Hanford Rd, Canton, MI 48187', 'Wayne County, Michigan'],
-    ['24624 W Warren St, Dearborn Heights, MI', 'Wayne County, Michigan'],
-    ['22100 Michigan Ave. Dearborn, MI 48124', 'Wayne County, Michigan'],
-    ['700 Broad Street St Joseph, MI 49085', ' Berrien County, Michigan'],
-    ['2724 Peck St, Muskegon Heights, MI 49444', 'Muskegon County, Michigan'],
-  ]
+const testCases: [string, string][] = [
+  ['100 S Biscayne Blvd, Miami, FL 33131', 'Miami-Dade County, Florida'],
+  ['5443 Main St, Port Richey, FL 34652', 'Pasco County, Florida'],
+  ['2000 W Commercial Blvd, Fort Lauderdale, FL 33309', 'Broward County, Florida'],
+  ['45525 Hanford Rd, Canton, MI 48187', 'Canton, Wayne County, Michigan'],
+  ['24624 W Warren St, Dearborn Heights, MI', 'Dearborn Heights, Wayne County, Michigan'],
+  ['22100 Michigan Ave. Dearborn, MI 48124', 'Dearborn, Wayne County, Michigan'],
+  ['700 Broad Street St Joseph, MI 49085', ' Berrien County, Michigan'],
+  ['2724 Peck St, Muskegon Heights, MI 49444', 'Muskegon County, Michigan'],
+]
 
+test('Snapshot names are unique', () => {
+  const snapNames = testCases.map((_, snapName) => snapName)
+  expect(snapNames).toHaveLength((new Set(snapNames)).size)
+})
+
+test('OSM is returning stable results', async () => {
   try {
     await Promise.all(
       testCases.map(async (pair) => {
-        const [addr, county] = pair
+        const [addr, snapName] = pair
         const result = await geocode(addr, '1A')
-        expect(result).toMatchSnapshot(county)
+        expect(result).toMatchSnapshot(snapName)
       })
     )
   } catch(e) {
