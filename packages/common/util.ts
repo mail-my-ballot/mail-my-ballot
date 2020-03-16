97b52cdbdb7666689c83a9024a7324612f4adf4b
Tianhui Michael Li <tianhuil@cs.princeton.edu>
2020-03-16 11:33:17 -0700

    Fixing TypeError

--- packages/common/util.ts
+++ packages/common/util.ts
@@ -2,5 +2,5 @@ export interface _Id {
   id?: string
 }
 
-export type WithoutId<T extends _Id> = Omit<T, 'id'>
-export type WithId<T extends _Id> = WithoutId<T> & {id: string}
+export type WithoutId<T> = T  extends _Id ? & Omit<T, 'id'> : never
+export type WithId<T> = T  extends _Id ? T & {id: string} : never
