03d7563623bd3097382cf7754892abe2783fcbfc
Tianhui Michael Li <tianhuil@cs.princeton.edu>
2020-03-15 17:52:11 -0700

    osm results

--- packages/client/src/lib/osm.test.ts
+++ packages/client/src/lib/osm.test.ts
@@ -5,6 +5,11 @@ test('OSM is returning stable results', async () => {
     ['100 S Biscayne Blvd, Miami, FL 33131', 'Miami-Dade County, Florida'],
     ['5443 Main St, Port Richey, FL 34652', 'Pasco County, Florida'],
     ['2000 W Commercial Blvd, Fort Lauderdale, FL 33309', 'Broward County, Florida'],
+    ['45525 Hanford Rd, Canton, MI 48187', 'Wayne County, Michigan'],
+    ['24624 W Warren St, Dearborn Heights, MI', 'Wayne County, Michigan'],
+    ['22100 Michigan Ave. Dearborn, MI 48124', 'Wayne County, Michigan'],
+    ['700 Broad Street St Joseph, MI 49085', ' Berrien County, Michigan'],
+    ['2724 Peck St, Muskegon Heights, MI 49444', 'Muskegon County, Michigan'],
   ]
 
   try {
