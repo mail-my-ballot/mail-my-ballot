35e30ca47cc392cea6c4bc72fdbe1331c886a390
Tianhui Michael Li <tianhuil@cs.princeton.edu>
2020-03-16 18:12:21 -0700

    using DEV_EMAIL in mg.proto.ts

--- packages/server/src/service/mg.proto.ts
+++ packages/server/src/service/mg.proto.ts
@@ -1,9 +1,12 @@
 import { sendEmail } from './mg'
 
-sendEmail({
-	to: ["tianhui.michael.li@gmail.com"],
-	subject: "Hello",
-	md: "**This is a test**",
-}).then(
-  (value) => console.log(value)
-)
+if (process.env.DEV_EMAIL) {
+  sendEmail({
+    to: [process.env.DEV_EMAIL],
+    subject: "Hello",
+    md: "**This is a test**",
+  }).then(
+    (value) => console.log(value)
+  )
+  
+}
