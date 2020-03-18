558b7235ca7f10ec8a142ed6b79b78d4a43e1db5
Tianhui Michael Li <tianhuil@cs.princeton.edu>
2020-03-17 21:58:12 -0700

    Make width fit within iPhone X

--- packages/client/src/comp/util/Signature.tsx
+++ packages/client/src/comp/util/Signature.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 import SignatureCanvas from 'react-signature-canvas'
 import styled from 'styled-components'
 
+const width = 350;
+
 const Label = styled.label`
   font-size: 12px;
   color: rgba(30, 134, 218, 0.918);;
@@ -9,7 +11,7 @@ const Label = styled.label`
 
 const BottomLine = styled.div`
   border-bottom: 1px solid rgba(0, 0, 0, 0.26);
-  width: 500px;
+  width: ${width}px;
 `
 
 type Props = React.PropsWithChildren<{
@@ -21,7 +23,7 @@ export const Signature = ({ inputRef, label }: Props) => (<div>
     <Label>{label}</Label>
     <BottomLine>
       <SignatureCanvas
-        canvasProps={{width: 500, height: 200}}
+        canvasProps={{width, height: 200}}
         ref={inputRef}
       />
     </BottomLine>
