bad583cb87a578a7e5138f1f3bf6cc816e5e2f55
Tianhui Michael Li <tianhuil@cs.princeton.edu>
2020-03-19 23:09:29 -0700

    Added more clear instructions to forward email

--- packages/client/src/comp/Success.tsx
+++ packages/client/src/comp/Success.tsx
@@ -8,8 +8,8 @@ export const Success = () => {
 
   return <>
     <h1>Congratulations!</h1>
-    <p>You have now successfully submitted a Vote by Mail application.</p>
-    <p>Check your email for the application (may be in spam) and &ldquo;Reply All&rdquo; to send it to your county official.</p>
+    <p>You have now successfully submitted a Vote by Mail application to your local elections official.</p>
+    <p>Check your inbox for the application email and hit <b>&ldquo;Reply All&rdquo;</b> to confirm with your local elections official.</p>
     { (confirmationId) ? (
       <p>Your Confirmation ID is <b>{confirmationId}</b>.  You may save off a copy for your records if you wish.</p>
     ) : (
