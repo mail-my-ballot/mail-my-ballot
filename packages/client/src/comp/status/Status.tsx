f31504023a1ebb70846bf7ecda11f5b028892310
Tianhui Michael Li <tianhuil@cs.princeton.edu>
2020-03-24 23:03:49 -0700

    Fix grammar in Status.tsx

--- packages/client/src/comp/status/Status.tsx
+++ packages/client/src/comp/status/Status.tsx
@@ -7,7 +7,7 @@ type StatusProps<T extends Status> = React.PropsWithChildren<T & StateField>
 export const Excuse = ({state}: StatusProps<Statuses.Excuse>) => (<>
   <h2 data-testid='status-title'>Sorry!</h2>
   <p data-testid='status-detail'>
-    You zipcode is in {state}.&nbsp;
+    Your zipcode is in {state}.&nbsp;
     {state} does not allow all registered voters to vote by mail.
     You may still qualify depending on your circumstances.
     For more information, visit your state election website.
@@ -17,7 +17,7 @@ export const Excuse = ({state}: StatusProps<Statuses.Excuse>) => (<>
 export const NoExcuse = ({state}: StatusProps<Statuses.NoExcuse>) => (<>
   <h2 data-testid='status-title'>Great News!</h2>
   <p data-testid='status-detail'>
-    You zipcode is in {state}.&nbsp;
+    Your zipcode is in {state}.&nbsp;
     {state} allows all registered voters to vote by mail.
     Unfortuantely, we do not yet support your State.
     For more information, visit your state election website.
@@ -27,7 +27,7 @@ export const NoExcuse = ({state}: StatusProps<Statuses.NoExcuse>) => (<>
 export const Automatic = ({state}: StatusProps<Statuses.Automatic>) => (<>
   <h2 data-testid='status-title'>Great News!</h2>
   <p data-testid='status-detail'>
-    You zipcode is in {state}.&nbsp;
+    Your zipcode is in {state}.&nbsp;
     All registered voters in {state} are automatically enrolled in vote by mail.
     For more information, visit your state election website.
   </p>
@@ -36,7 +36,7 @@ export const Automatic = ({state}: StatusProps<Statuses.Automatic>) => (<>
 export const Website = ({state, regUrl, infoUrl}: StatusProps<Statuses.Website>) => (<>
   <h2 data-testid='status-title'>Great News!</h2>
   <p data-testid='status-detail'>
-    You zipcode is in {state}.&nbsp;
+    Your zipcode is in {state}.&nbsp;
     {state} allows registered voters to vote by mail.
     You can apply on the <a href={regUrl}>official state election application page</a>.
     For more information, visit your <a href={infoUrl}>state election website</a>.
@@ -46,7 +46,7 @@ export const Website = ({state, regUrl, infoUrl}: StatusProps<Statuses.Website>)
 export const Mail = ({state, infoUrl}: StatusProps<Statuses.Mail>) => (<>
   <h2 data-testid='status-title'>Great News!</h2>
   <p data-testid='status-detail'>
-    You zipcode is in {state}.&nbsp;
+    Your zipcode is in {state}.&nbsp;
     {state} allows registered voters to vote by mail.
     However, the state requires mailing a physical application, which we cannot support.
     For more information, visit your <a href={infoUrl}>state election website</a>.
@@ -56,7 +56,7 @@ export const Mail = ({state, infoUrl}: StatusProps<Statuses.Mail>) => (<>
 export const VbmApp = ({state, children}: StatusProps<Statuses.VbmApp>) => (<>
   <h2 data-testid='status-title'>Great News!</h2>
   <p data-testid='status-detail'>
-    You zipcode is in {state}.&nbsp;
+    Your zipcode is in {state}.&nbsp;
     {state} allows registered voters to vote by mail and we can help you enroll.
   </p>
   { children }
@@ -65,7 +65,7 @@ export const VbmApp = ({state, children}: StatusProps<Statuses.VbmApp>) => (<>
 export const Unidentified = ({state}: StateField) => (<>
   <h2 data-testid='status-title'>Sorry!</h2>
   <p data-testid='status-detail'>
-    You zipcode is in {state}.&nbsp;
+    Your zipcode is in {state}.&nbsp;
     Unfortunately, We do not have any information on vot by mail for {state}.
   </p>
 </>)
