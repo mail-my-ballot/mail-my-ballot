0cdd86e0eb4a4f5dc9c41476ccf083b6cf43d771
Tianhui Michael Li <tianhuil@cs.princeton.edu>
2020-03-26 10:36:28 -0700

    warning message no style container

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -42,9 +42,7 @@ const Layout = () => {
         <ScrollHook pathEnum='start'>
           <Blurb/>
         </ScrollHook>
-        <StyleContainer>
-          <WarningMsg/>
-        </StyleContainer>
+        <WarningMsg/>
       </Route>
       <Route path={pathData['success'].path}>
         <ScrollHook pathEnum='success'>
