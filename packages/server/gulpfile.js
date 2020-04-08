883e4d557c0f07a5d165dae14a31f0bc2d178c63
Tianhui Michael Li <tianhuil@cs.princeton.edu>
2020-04-08 09:47:00 -0700

    gulp-run-command now accepts input

--- packages/server/gulpfile.js
+++ packages/server/gulpfile.js
@@ -1,7 +1,10 @@
 const gulp = require('gulp')
 const minimist = require('minimist')
-const run = require('gulp-run-command').default
+const run = require('@tianhuil/gulp-run-command').default
+const env = require('./env/.env.js')
+// const shell = require('gulp-shell')
 
 const options = minimist(process.argv.slice(2), {})
 
-gulp.task('test', run('echo hello'))
+gulp.task('test', async () => run('jest \\.test\\.ts --watchAll')())
+gulp.task('start', async () => run('ts-node-dev --respawn --transpileOnly src/index.ts', { env: env.development })())
