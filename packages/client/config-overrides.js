0c8cb78bdec5c4c1d90b95997b97a7345a1c54a8
Tianhui Michael Li <tianhuil@cs.princeton.edu>
2020-04-16 16:57:02 -0700

    Add env to exclude

--- packages/client/config-overrides.js
+++ packages/client/config-overrides.js
@@ -2,5 +2,6 @@ module.exports = function override(config, env) {
   //do stuff with the webpack config...
   config;
   config.resolve.symlinks = false
+  config.exclude = ['env/']
   return config
 }
