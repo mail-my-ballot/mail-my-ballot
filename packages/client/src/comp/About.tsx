beddb6f927f2750358a4e615575cf923981528d0
Tianhui Michael Li <tianhuil@cs.princeton.edu>
2020-04-20 17:44:05 -0700

    Adding a message about project

--- packages/client/src/comp/About.tsx
+++ packages/client/src/comp/About.tsx
@@ -15,6 +15,9 @@ export const About = () => {
       <Col xs={12} md={8} md-offset={2} lg={6} lg-offset={3}>
         <h1>About Us</h1>
         <p>
+          COVID-19 has catalyzed interest in Vote by Mail (VBM).  <a href={url}>{brandName}</a> streamlines government VBM applications by digitizing the voter’s VBM signup process.  The mobile-friendly website drives voter turnout in key swing states by helping organizers sign up voters for VBM.
+        </p>
+        <p>
           <a href={url}>{brandName}</a> is a <a href='https://voteathome.org'>Vote at Home</a> project.
           Vote at Home is a non-partisan 501(c)3 that empowers voters, letting them decide when, how and where they vote.
         </p>
