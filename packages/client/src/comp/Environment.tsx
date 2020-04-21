fb6cbcb551ef222b151af6cb3f96eb5137a62892
Tianhui Michael Li <tianhuil@cs.princeton.edu>
2020-04-21 11:10:26 -0700

    Make environment variable more clear

--- packages/client/src/comp/Environment.tsx
+++ packages/client/src/comp/Environment.tsx
@@ -5,6 +5,6 @@ export const Environment = () => <>
   <ul>
     <li>NODE_ENV: {process.env.NODE_ENV}</li>
     <li>REACT_APP_ENVIRONMENT: {process.env.REACT_APP_ENVIRONMENT}</li>
-    <li>REACT_APP_EMAIL_OFFICIALS: {!!process.env.REACT_APP_EMAIL_OFFICIALS}</li>
+    <li>REACT_APP_EMAIL_OFFICIALS: {process.env.REACT_APP_EMAIL_OFFICIALS}</li>
   </ul>
 </>
