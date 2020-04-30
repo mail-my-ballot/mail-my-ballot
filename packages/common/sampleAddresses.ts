8912fe2d96eb5447b5a9f7d886b518f7d6f3d75c
Tianhui Michael Li <tianhuil@cs.princeton.edu>
2020-04-30 14:03:01 -0700

    Adding zipcode for Michigan address

--- packages/common/sampleAddresses.ts
+++ packages/common/sampleAddresses.ts
@@ -18,7 +18,7 @@ export const sampleAddresses: [string, string][] = [
   ['5443 Main St, Port Richey, FL 34652', 'Pasco County, Florida'],
   ['2000 W Commercial Blvd, Fort Lauderdale, FL 33309', 'Broward County, Florida'],
   ['45525 Hanford Rd, Canton, MI 48187', 'Canton, Wayne County, Michigan'],
-  ['24624 W Warren St, Dearborn Heights, MI', 'Dearborn Heights, Wayne County, Michigan'],
+  ['24624 W Warren St, Dearborn Heights 48127, MI', 'Dearborn Heights, Wayne County, Michigan'],
   ['22100 Michigan Ave. Dearborn, MI 48124', 'Dearborn, Wayne County, Michigan'],
   ['700 Broad Street St Joseph, MI 49085', 'Berrien County, Michigan'],
   ['2724 Peck St, Muskegon Heights, MI 49444', 'Muskegon County, Michigan'],
