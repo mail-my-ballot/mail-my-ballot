8fc0de8f2da6107b31e68d7778966658a132d9ab
Tianhui Michael Li <tianhuil@cs.princeton.edu>
2020-04-30 17:58:07 -0700

    Add in full snapshot tests

---
+++ packages/client/src/lib/osm.test.ts
@@ -0,0 +1,22 @@
+import { geocode } from './osm'
+import { sampleAddresses } from '../common/sampleAddresses'
+
+test('Snapshot names are unique', () => {
+  const snapNames = sampleAddresses.map((_, snapName) => snapName)
+  expect(snapNames).toHaveLength((new Set(snapNames)).size)
+})
+
+const wait = (ms: number) => new Promise(resolve => setTimeout(resolve, ms))
+
+test('OSM is returning stable results', async (cb) => {
+  jest.setTimeout(30000)
+  await Promise.all(
+    sampleAddresses.map(async ([addr, snapName], index) => {
+      await wait(index * 800)
+      const result = await geocode(addr, '1A')
+      expect(result).toMatchSnapshot(snapName)
+    })
+  )
+  jest.setTimeout(5000)
+  cb()
+})
