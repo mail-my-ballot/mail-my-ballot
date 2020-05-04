8301e00e0897693075c2973e15fb20a7a636f2d3
Tianhui Michael Li <tianhuil@cs.princeton.edu>
2020-05-04 11:49:50 -0700

    Using test.each

--- packages/server/src/service/contact/loader.test.ts
+++ packages/server/src/service/contact/loader.test.ts
@@ -1,17 +1,23 @@
 import { loadStates } from './loader'
 import { normalize } from './normalize'
 import { availableStates } from '../../common'
+import { RawContactRecord, ContactRecord } from './type'
 
-test.only('Contact loader', async () => {
-  const rawRecords = await loadStates()
-  availableStates.forEach(state => {
-    expect(rawRecords[state].length).toBeGreaterThan(10)
+describe('Contact loader', () => {
+  let rawRecords: RawContactRecord
+  let records: ContactRecord
+  beforeAll(async () => {
+    rawRecords = await loadStates()
+    records = normalize(rawRecords)
   })
+
+  test.each(availableStates)(
+    `Non-trivial raw records for %i`,
+    (state) => expect(rawRecords[state].length).toBeGreaterThan(10)
+  )
   
-  const records = normalize(rawRecords)
-  availableStates.forEach(state => {
-    expect(Object.keys(records[state]).length).toBeGreaterThan(10)
-  })
-  // const foo = subAdapter('Florida', rawRecords['Florida'])
-  // expect(Object.keys(foo).length).toBeGreaterThan(10)
+  test.each(availableStates)(
+    `Non-trivial normalized records for %i`,
+    state => expect(Object.keys(records[state]).length).toBeGreaterThan(10)
+  )
 })
