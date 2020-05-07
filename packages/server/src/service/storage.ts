45a71d74e3250da28646e7864930bc35d5ca8d9b
Tianhui Michael Li <tianhuil@cs.princeton.edu>
2020-05-06 17:13:03 -0700

    Tweak storage

--- packages/server/src/service/storage.ts
+++ packages/server/src/service/storage.ts
@@ -1,42 +1,47 @@
 import stream from 'stream'
 
-import { Storage } from '@google-cloud/storage'
+import { Storage, File } from '@google-cloud/storage'
 import { processEnvOrThrow } from '../../../client/src/common'
 
 const storage = new Storage()
 const bucket = processEnvOrThrow('GOOGLE_STORAGE_BUCKET')
 
-const makeFile = (filename: string) => {
-  return storage.bucket(bucket).file(filename)
-}
+export class StorageFile {
+  filename: string
+  file: File
 
-export const upload = async (filename: string, data: string | Buffer) => {
-  const bufferStream = new stream.PassThrough()
-  const buffer = (data instanceof Buffer) ? data : Buffer.from(data)
-  bufferStream.end(buffer)
+  constructor(filename: string) {
+    this.filename = filename
+    this.file = storage.bucket(bucket).file(filename)
+  }
 
-  const file = makeFile(filename)
+  async upload(data: string | Buffer) {
+    const bufferStream = new stream.PassThrough()
+    const buffer = (data instanceof Buffer) ? data : Buffer.from(data)
+    bufferStream.end(buffer)
 
-  const writable = bufferStream.pipe(file.createWriteStream({
-    metadata: {
-      contentType: 'application/pdf',
+  
+    const writable = bufferStream.pipe(this.file.createWriteStream({
       metadata: {
-        custom: 'metadata'
-      }
-    },
-    public: false,
-    validation: 'md5'
-  }))
+        contentType: 'application/pdf',
+        metadata: {
+          custom: 'metadata'
+        }
+      },
+      public: false,
+      validation: 'md5'
+    }))
+  
+    return new Promise((resolve, reject) => {
+      writable.on('finish', () => resolve(true))
+      writable.on('error', reject)
+    })  
+  }
 
-  return new Promise((resolve, reject) => {
-    writable.on('finish', () => resolve(true))
-    writable.on('error', reject)
-  })  
-}
-
-export const getSignedUrl = async (filename: string, durationMs: number) => {
-  return makeFile(filename).getSignedUrl({
-    action: 'read',
-    expires: new Date(new Date().getTime() + durationMs)
-  })
+  getSignedUrl(durationMs: number) {
+    return this.file.getSignedUrl({
+      action: 'read',
+      expires: new Date(new Date().getTime() + durationMs)
+    })
+  }
 }
