ccefb4cbed0f90640b6cc64ff09d03982bb42042
Tianhui Michael Li <tianhuil@cs.princeton.edu>
2020-05-08 18:32:58 -0700

    Dropdown menus in Warning Message

--- packages/client/src/comp/WarningMsg.tsx
+++ packages/client/src/comp/WarningMsg.tsx
@@ -1,13 +1,17 @@
 import React from 'react'
-import Tabs from 'muicss/lib/react/tabs'
-import Tab from 'muicss/lib/react/tab'
+import Dropdown from 'muicss/lib/react/dropdown'
+import DropdownItem from 'muicss/lib/react/dropdown-item'
 
 import { RedOutline } from './util/RedOutline'
 import { sampleAddresses } from '../common/sampleAddresses'
+import { AddressContainer } from '../lib/state'
+import { State } from '../common'
 
 
 const RawWarningMsg = () => {
   const states = Array.from(new Set(sampleAddresses.map(address => address.state)))
+  const { locale } = AddressContainer.useContainer()
+  const [state, setState] = React.useState<State>(locale?.state ?? 'Florida')
   
   return (<RedOutline>
     <h3>Warning: Not Production!</h3>
@@ -17,38 +21,43 @@ const RawWarningMsg = () => {
       No email is sent to a local elections official so you can safely play with this demo.
     </p>
     <h4>Filling out the form:</h4>
-    <p><b>Address:</b> You can fill this out with any address but to see it in action, you will need to use an address in the state.  Sample addresses are listed below.</p>
+    <p><b>Address:</b> You can fill this out with any address.  But to see it in action, you will want to use an address in a state we support.  Sample addresses are listed below.</p>
     <p><b>Email:</b> When prompted, please use your own email (so as to not spam others!)</p>
-    
-    <Tabs
-      defaultSelectedIndex={0}
+
+    <Dropdown
+      label={state}
+      color='primary'
       style={{marginTop: '2em'}}
     >
       {
-        states.map((state, key) => {
-          return <Tab key={key} value={state} label={state}>
-            <ul style={{marginTop: '1em'}}>
-              {sampleAddresses
-                .filter(addrData => addrData.state === state )
-                .map((addrData, key) => <li key={key}>
-                  {addrData.address} ({addrData.city}, {addrData.county}, {addrData.state})
-                  <button
-                    style={{marginLeft: '1em'}}
-                    onClick={() => {
-                      const input = document.getElementById('addr-input') as HTMLInputElement
-                      input.value = addrData.address
-                      document.getElementById('addr-submit')?.click()
-                    }}
-                  >
-                    Fill
-                  </button>
-                  </li>
-                )}
-            </ul>
-          </Tab>
+        states.sort().map((state, key) => {
+          return <DropdownItem
+            key={key}
+            onClick={() => setState(state)}
+          >
+            {state}
+          </DropdownItem>
         })
       }
-    </Tabs>
+    </Dropdown>
+    <ul style={{marginTop: '1em'}}>
+      {sampleAddresses
+        .filter(addrData => addrData.state === state )
+        .map((addrData, key) => <li key={key}>
+          {addrData.address} ({addrData.city}, {addrData.county}, {addrData.state})
+          <button
+            style={{marginLeft: '1em'}}
+            onClick={() => {
+              const input = document.getElementById('addr-input') as HTMLInputElement
+              input.value = addrData.address
+              document.getElementById('addr-submit')?.click()
+            }}
+          >
+            Fill
+          </button>
+        </li>
+      )}
+    </ul>
 
   </RedOutline>)
 }
