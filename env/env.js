f3de8c1505c5592e40de869c02487a4808ce3096
Tianhui Michael Li <tianhuil@cs.princeton.edu>
2020-05-10 14:05:53 -0700

    Adding env.js to staging

--- env/env.js
+++ env/env.js
@@ -36,7 +36,7 @@ const base = removeNullValues({
   MG_DOMAIN: 'email.mailmyballot.org',
   MG_FROM_ADDR: 'Vote by Mail Application <application@email.mailmyballot.org>',
   MG_REPLY_TO_ADDR: 'Vote by Mail Application <application@mailmyballot.org>',
-  ELECTIONS_OFFICIALS_VERSION: 'v1.3.5',
+  ELECTIONS_OFFICIALS_VERSION: 'v1.3.6',
 })
 
 const development = removeNullValues({
