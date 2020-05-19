8e36ec178ed288baa8fd4fbf108396869683108d
Tianhui Michael Li <tianhuil@cs.princeton.edu>
2020-05-18 23:06:00 -0700

    Testing Upload Button

---
+++ packages/client/src/comp/util/UploadButton.test.tsx
@@ -0,0 +1,69 @@
+import React from 'react'
+import 'jest-canvas-mock'
+import { UploadButton } from './UploadButton'
+
+import { render, fireEvent, wait, act } from '@testing-library/react'
+import { mocked } from 'ts-jest/utils'
+
+describe('UploadButton', () => {
+  const setup = (maxSizeMB: number) => {
+    const setDataString = jest.fn<void, [string]>()
+    const { getByTestId } = render(<UploadButton
+      label='label'
+      setDataString={setDataString}
+      maxSizeMB={maxSizeMB}
+    />)
+
+    const clickInputSpy = jest.spyOn(HTMLInputElement.prototype, 'click')
+        mocked(window, true).alert = jest.fn()
+
+    return {clickInputSpy, getByTestId, setDataString}
+  }
+
+  const upload = (el: HTMLElement) => {
+    act(() => {
+      fireEvent.change(el, {
+        target: {
+          files: [
+            new File(['somedata'], 'file.jpg')
+          ] 
+        }
+      })
+    })
+  }
+
+  test('Click triggers file callback', async () => {
+    const { clickInputSpy, getByTestId } = setup(1)
+
+    act(() => {
+      fireEvent.click(getByTestId('upload-click-target'), {
+        bubbles: true,
+        cancelable: true,
+      })
+    })
+
+    await wait(() => expect(clickInputSpy).toHaveBeenCalledTimes(1))
+  })
+
+  test('Upload file triggers', async () => {
+    const { setDataString, getByTestId } = setup(1)
+
+    await wait(() => expect(setDataString).toHaveBeenCalledTimes(0))
+    
+    upload(getByTestId('upload-input'))
+
+    await wait(() => expect(window.alert).toHaveBeenCalledTimes(0))
+    await wait(() => expect(setDataString).toHaveBeenCalledTimes(1))
+  })
+
+  test('Max size stops upload', async () => {
+    const { setDataString, getByTestId }  = setup(5./1024/1024)
+
+    await wait(() => expect(setDataString).toHaveBeenCalledTimes(0))
+    
+    upload(getByTestId('upload-input'))
+
+    await wait(() => expect(window.alert).toHaveBeenCalledTimes(1))
+    await wait(() => expect(setDataString).toHaveBeenCalledTimes(0))
+  })
+})
--- packages/client/src/comp/util/UploadButton.tsx
+++ packages/client/src/comp/util/UploadButton.tsx
@@ -17,6 +17,7 @@ interface Props {
   label: string
   setDataString: (dataString: string) => void
   required?: boolean
+  maxSizeMB?: number
 }
 
 const style: React.CSSProperties = {
@@ -38,7 +39,8 @@ interface Image {
 export const UploadButton: React.FC<Props> = ({
   label,
   setDataString,
-  required
+  required,
+  maxSizeMB,
 }) => {
   const ref = React.useRef<HTMLInputElement | null>()
   const [image, setImage] = React.useState<Image>()
@@ -48,11 +50,13 @@ export const UploadButton: React.FC<Props> = ({
     if (ref.current) ref.current.click()
   }
 
+  const maxSizeMBReal = maxSizeMB ?? 1
+
   const onChange = async (event: React.ChangeEvent<HTMLInputElement>) => {
     if (event.target.files && event.target.files.length > 0) {
       const file = event.target.files[0]
-      if (file.size > 1024 * 1024) {
-        window.alert('File size is limited to 1MB')
+      if (file.size > (maxSizeMBReal * 1024 * 1024)) {
+        window.alert(`File size is limited to ${maxSizeMBReal}MB`)
       } else {
         const data = await toDataUrl(file)
         setImage({name: file.name, data: data})
@@ -70,7 +74,7 @@ export const UploadButton: React.FC<Props> = ({
 
   return <div>
     <div onClick={onClick} style={style}>
-      <div>
+      <div data-testid='upload-click-target'>
         {
           (image) ? <>
               <img src={image.data} style={{maxHeight: '150px', ...centerBlock}} alt='thumbnail'/>
@@ -79,13 +83,14 @@ export const UploadButton: React.FC<Props> = ({
             : <>{
               // eslint-disable-next-line
               }<h1 style={{marginTop: '0', ...centerBlock}}><i className="fa fa-upload" aria-hidden="true"/></h1>
-                <p style={centerBlock}>Limit: 1MB</p>
+                <p style={centerBlock}>Limit: ${maxSizeMBReal}MB</p>
               </>
         }
         <RoundedButton color='primary' style={centerBlock} >{label}</RoundedButton>
       </div>
     </div>
     <input
+      data-testid='upload-input'
       name='upload'
       type='file'
       style={{opacity: '0', height: '1px' }}
