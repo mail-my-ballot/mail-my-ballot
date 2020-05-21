cb0c333d5ff9a456b65c8a72202ee36d5360ed2c
Tianhui Michael Li <tianhuil@cs.princeton.edu>
2020-05-20 18:06:55 -0700

    fetch data producers more organized reports

--- packages/server/src/script/fetchData.ts
+++ packages/server/src/script/fetchData.ts
@@ -1,21 +1,38 @@
 import fs from 'fs'
 
 import { FirestoreService } from "../service/firestore"
-import { RichStateInfo } from "../service/types"
+import { RichStateInfo, Org, User } from "../service/types"
 
 const firestore = new FirestoreService()
 const main = async() => {
   const env = process.argv[2]
-  const infos = await firestore.query<RichStateInfo>(
-    firestore.db.collection('StateInfo')
-      .orderBy('created', 'asc')
-  )
+
+  const [
+    stateInfo,
+    user,
+    org,
+  ] = await Promise.all([
+    firestore.query<RichStateInfo>(
+      firestore.db.collection('StateInfo')
+        .orderBy('created', 'asc')
+    ),
+    firestore.query<User>(firestore.db.collection('User')),
+    firestore.query<Org>(firestore.db.collection('Org')),
+  ])
   
   const date = new Date().toISOString()
+
   const filename = __dirname + `/data/infos-${env}-${date}.json`
-  fs.writeFileSync(filename, JSON.stringify(infos))
+  fs.writeFileSync(filename, JSON.stringify({
+    stateInfo,
+    user,
+    org,
+  }))
   
-  console.log(`${infos.length} results written to ${filename}`)
+  console.log(`Writing results to ${filename}`)
+  console.log(`${stateInfo.length} voters written`)
+  console.log(`${user.length} users written`)
+  console.log(`${org.length} orgs written`)
 }
 
 main()
