cc6b68949b6714c4dee6bbf06072b06d7eba718e
Tianhui Michael Li <tianhuil@cs.princeton.edu>
2020-05-24 13:33:51 -0700

    More clear geocoding errors

--- packages/server/src/service/gm.proto.ts
+++ packages/server/src/service/gm.proto.ts
@@ -3,7 +3,10 @@ import { cache } from './util'
 
 const main = async() => {
   const geoResult = await cache(rawGeocode)('132 N Royal St, Alexandria, VA 22314-3246')
-  if (!geoResult) return
+  if (!geoResult) {
+    console.log('No Result Returned')
+    return
+  }
   console.log(JSON.stringify(geoResult, null, 2))
   const address = await toAddress(geoResult)
   console.log(address)
