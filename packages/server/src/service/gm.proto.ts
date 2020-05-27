909e3902dfeb4c442a627b2088385a013dd42918
Tianhui Michael Li <tianhuil@cs.princeton.edu>
2020-05-27 15:52:32 -0700

    Update gm.proto.ts

--- packages/server/src/service/gm.proto.ts
+++ packages/server/src/service/gm.proto.ts
@@ -1,5 +1,7 @@
 import { rawGeocode, toAddress } from './gm'
 import { cache } from './util'
+import { toContact } from './contact'
+import { Locale, toContactMethod } from '../common'
 
 const main = async() => {
   const geoResult = await cache(rawGeocode)('132 N Royal St Apt #34, Alexandria, VA 22314-3246')
@@ -10,6 +12,14 @@ const main = async() => {
   console.log(JSON.stringify(geoResult, null, 2))
   const address = await toAddress(geoResult)
   console.log(address)
+
+  if (!address) return
+  const contact = await toContact(address as Locale)
+  console.log(contact)
+
+  if (!contact) return
+  const method = await toContactMethod(contact)
+  console.log(method)
 }
 
 main()
