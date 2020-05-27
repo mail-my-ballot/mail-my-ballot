688eb0065e0d77bb1723063d409cdbd5b2474136
Lukas Danin <lukas.danin@gmail.com>
2020-05-27 15:41:21 -0300

    Adds ToastContainer

--- packages/client/src/comp/StateContainer.tsx
+++ packages/client/src/comp/StateContainer.tsx
@@ -1,6 +1,15 @@
 import React from 'react'
 import { HashRouter } from 'react-router-dom'
+import { Slide, ToastContainer } from 'react-toastify'
 import { QueryContainer, AddressContainer, ContactContainer, AnalyticsContainer, VoterContainer, ZipFinderContainer } from '../lib/unstated'
+import 'react-toastify/dist/ReactToastify.css'
+
+const toastPosition = () => {
+  return window.matchMedia('(max-width: 991px)').matches
+    ? 'top-center'
+    : 'bottom-right'
+}
+
 // export for testing purposes
 export const UnstatedContainer: React.FC<{}> = ({ children }) => (<HashRouter>
   <ZipFinderContainer.Provider>
@@ -10,6 +19,17 @@ export const UnstatedContainer: React.FC<{}> = ({ children }) => (<HashRouter>
           <AnalyticsContainer.Provider>
             <VoterContainer.Provider>
               {children}
+              <ToastContainer
+                position={toastPosition()}
+                autoClose={5000}
+                hideProgressBar={true}
+                newestOnTop={true}
+                closeOnClick={true}
+                rtl={false}
+                pauseOnFocusLoss={true}
+                pauseOnHover={true}
+                transition={Slide}
+              />
             </VoterContainer.Provider>
           </AnalyticsContainer.Provider>
         </ContactContainer.Provider>
