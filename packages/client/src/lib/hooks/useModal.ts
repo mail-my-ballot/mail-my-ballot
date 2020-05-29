ae2b5c4c4199f2a9ab517e219223dcdcd8490f20
rbprojects7 <53977855+rbprojects7@users.noreply.github.com>
2020-05-29 10:09:09 +0200

    fix useModal hook default value

--- packages/client/src/lib/hooks/useModal.ts
+++ packages/client/src/lib/hooks/useModal.ts
@@ -2,8 +2,8 @@ import { useState } from 'react'
 
 // custom useModal hook
 export default () => {
-  const [modal, setModal] = useState(false)
-
+  const [modal, setModal] = useState(true)
+  
   const handleModal = () => setModal(!modal)
 
   return { modal, handleModal }
