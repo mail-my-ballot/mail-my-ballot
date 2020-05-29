fe09ecac1b1d35e6937237cf3fd31bb0a4304358
Tianhui Michael Li <tianhuil@cs.princeton.edu>
2020-05-29 00:10:44 -0700

    remove redundant HTML

--- packages/server/src/service/pdf.ts
+++ packages/server/src/service/pdf.ts
@@ -2,8 +2,7 @@ import pdf from 'html-pdf'
 
 export const toPdfBuffer = async (html: string): Promise<Buffer> => {
   return new Promise((resolve, reject) => {
-    const wrappedHtml = html
-    pdf.create(wrappedHtml, { format: 'Letter', border: '72px' }).toBuffer((err, buffer) => {
+    pdf.create(html, { format: 'Letter', border: '72px' }).toBuffer((err, buffer) => {
       if (err) reject(err)
       resolve(buffer)
     })
