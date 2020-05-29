021a750b30a3e134e5d6dad0d41c5f7039786602
Tianhui Michael Li <tianhuil@cs.princeton.edu>
2020-05-29 11:52:24 -0700

    Update storage.proto.ts

--- packages/server/src/service/storage.proto.ts
+++ packages/server/src/service/storage.proto.ts
@@ -1,12 +1,14 @@
-import { upload, getSignedUrl } from './storage'
+import { StorageFile } from './storage'
 import fs from 'fs'
 
 const main = async () => {
   const buffer = fs.readFileSync('test.pdf')
-  const result = await upload('test/test.pdf', buffer)
+  const file = new StorageFile('test/test.pdf')
+  const result = await file.upload(buffer)
   console.log(result)
-  const reuslt2 = await getSignedUrl('test/test.pdf', 24 * 60 * 60 * 1000)
+  const reuslt2 = await file.getSignedUrl(24 * 60 * 60 * 1000)
   console.log(reuslt2)
+
 }
 
 main()
