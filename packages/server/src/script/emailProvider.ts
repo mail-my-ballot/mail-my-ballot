cb2b9e0972ff2b3b5e01a09dca48697dbde8aa30
Tianhui Michael Li <tianhuil@cs.princeton.edu>
2020-06-07 23:01:28 -0700

    gm / contact

---
+++ packages/server/src/script/emailProvider.ts
@@ -0,0 +1,20 @@
+import DataFrame from 'dataframe-js'
+
+import { FirestoreService } from "../service/firestore"
+import { RichStateInfo } from "../service/types"
+
+const firestore = new FirestoreService()
+const main = async() => {
+  const users = await (firestore.query<RichStateInfo>(
+    firestore.db.collection('StateInfo')
+      .orderBy('created', 'asc')
+  ))
+  const df = new DataFrame(users, ['name', 'email', 'oid'])
+  const res = df.groupBy('oid')
+    .aggregate((group) => group.count())
+    .rename('aggregation', 'oidCount')
+    .toCSV(true)
+  console.log(res)
+}
+
+main()
