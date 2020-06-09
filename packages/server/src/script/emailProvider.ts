7b3e0000eeddbfc61c43a2c817943b669d01bff9
Tianhui Michael Li <tianhuil@cs.princeton.edu>
2020-06-08 18:45:10 -0700

    Basic queries

--- packages/server/src/script/emailProvider.ts
+++ packages/server/src/script/emailProvider.ts
@@ -1,26 +1,24 @@
-import DataFrame from 'dataframe-js'
+import { DataFrame } from 'data-forge'
 
 import { FirestoreService } from "../service/firestore"
 import { RichStateInfo } from "../service/types"
 
 const firestore = new FirestoreService()
 const main = async() => {
-  const users = await (firestore.query<RichStateInfo>(
-    firestore.db.collection('StateInfo')
-      .orderBy('created', 'asc')
-  ))
-
-  const users = await (firestore.query<RichStateInfo>(
+  const stateInfo = await (firestore.query<RichStateInfo>(
     firestore.db.collection('StateInfo')
       .orderBy('created', 'asc')
   ))
   
-  const df = new DataFrame(users, ['name', 'email', 'oid'])
-  const res = df.groupBy('oid')
-    .aggregate((group) => group.count())
-    .rename('aggregation', 'oidCount')
-    .toCSV(true)
-  console.log(res)
+  const df = new DataFrame(stateInfo)
+  const csv = df.groupBy(row => row.oid)
+    .select((group) => ({
+      oid: group.first().oid,
+      count: group.count(),
+    }))
+    .inflate()
+    .toCSV()
+  console.log(csv)
 }
 
 main()
