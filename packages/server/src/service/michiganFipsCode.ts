9fe5c3dbc7151b24aef4c2d322f8f1b083c10ccd
Tianhui Michael Li <tianhuil@cs.princeton.edu>
2020-06-09 15:36:58 -0700

    Catch Michigan Error

--- packages/server/src/service/michiganFipsCode.ts
+++ packages/server/src/service/michiganFipsCode.ts
@@ -9,10 +9,15 @@ interface Response {
   }[]
 }
 
-const rawMichiganResponse = async (latLong: [number, number]): Promise<Response> => {
+const rawMichiganResponse = async (latLong: [number, number]): Promise<Response | null> => {
   const [lat, lng] = latLong
   const url = `https://gisago.mcgi.state.mi.us/arcgis/rest/services/OpenData/michigan_geographic_framework/MapServer/2/query?where=1%3D1&geometry=${lng}%2C${lat}&geometryType=esriGeometryPoint&inSR=4326&spatialRel=esriSpatialRelIntersects&outFields=*&returnGeometry=true&returnTrueCurves=false&returnIdsOnly=false&returnCountOnly=false&returnZ=false&returnM=false&returnDistinctValues=false&returnExtentOnly=false&featureEncoding=esriDefault&f=pjson`
-  return (await (await fetch(url)).json() as Response)
+  try {
+    return (await (await fetch(url)).json() as Response)
+  } catch (error) {
+    console.warn(`Unable to fetch Michigan ArcGIS service: ${JSON.stringify(error, null, 2)}`)
+    return null
+  }
 }
 
 export const toFipscode = async (response: Response): Promise<string | null> => {
@@ -27,5 +32,6 @@ export const michiganFipsCode = async (
 ): Promise<string | null> => {
   const call = cacheQuery ? cacheMichiganResponse : rawMichiganResponse
   const response = await call(latLong)
+  if (!response) return null
   return toFipscode(response)
 }
