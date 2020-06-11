13c133008f89b71b6ced36c71e1b3df741152822
Lukas Danin <lukas.danin@gmail.com>
2020-06-11 20:27:34 -0300

    Implements react-toastify

--- packages/client/src/comp/StateContainer.tsx
+++ packages/client/src/comp/StateContainer.tsx
@@ -1,8 +1,18 @@
 import React from 'react'
 import { HashRouter } from 'react-router-dom'
+import { Slide, ToastContainer } from "react-toastify"
 import { ModalProvider } from 'styled-react-modal'
 import { QueryContainer, AddressContainer, ContactContainer, AnalyticsContainer, VoterContainer, FeatureFlagsContainer } from '../lib/unstated'
 
+import 'react-toastify/dist/ReactToastify.css'
+
+// For some reason on the current design toasts placed on the botton are
+// not shown on top of the Blurb
+const toastPosition = () => {
+  return window.matchMedia('(max-width: 991px)').matches
+    ? 'top-center'
+    : 'bottom-right'
+}
 
 // export for testing purposes
 export const UnstatedContainer: React.FC<{}> = ({ children }) => (<HashRouter>
@@ -14,6 +24,17 @@ export const UnstatedContainer: React.FC<{}> = ({ children }) => (<HashRouter>
             <VoterContainer.Provider>
               <ModalProvider>
                 {children}
+                <ToastContainer
+                  position={toastPosition()}
+                  autoClose={5000}
+                  hideProgressBar={true}
+                  newestOnTop={true}
+                  closeOnClick={true}
+                  rtl={false}
+                  pauseOnFocusLoss={true}
+                  pauseOnHover={true}
+                  transition={Slide}
+                />
               </ModalProvider>
             </VoterContainer.Provider>
           </FeatureFlagsContainer.Provider>
