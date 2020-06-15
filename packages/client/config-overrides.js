c68c379584047bb1a99b833cdedd66e8baa5f375
Marco Carvalho <marcoaureliocarvalhofilho@hotmail.com>
2020-06-15 16:38:30 -0300

    No hashtag and webpack tweak

--- packages/client/config-overrides.js
+++ packages/client/config-overrides.js
@@ -2,5 +2,16 @@ module.exports = function override(config, env) {
   //do stuff with the webpack config...
   config;
   config.resolve.symlinks = false
+  config.optimization = {
+    splitChunks: {
+      cacheGroups: {
+        commons: {
+          test: /[\\/]node_modules[\\/]/,
+          name: 'vendors',
+          chunks: 'all'
+        }
+      }
+    }
+  }
   return config
 }
