ace9c974699c278b0dd3f7de8c71b0583e4eeec8
Tianhui Michael Li <tianhuil@cs.princeton.edu>
2020-06-17 13:48:42 -0700

    Test all utm fields

--- packages/client/src/comp/Initialize.test.tsx
+++ packages/client/src/comp/Initialize.test.tsx
@@ -5,15 +5,27 @@ import { Initialize } from './Initialize'
 import { UnstatedContainer } from './StateContainer'
 
 test('Initialize picks up parameters', () => {
+  const utmFields = {
+    utm_source: 'source1234',      //eslint-disable-line @typescript-eslint/camelcase
+    utm_medium: 'medium1234',      //eslint-disable-line @typescript-eslint/camelcase
+    utm_campaign: 'campaign1234',  //eslint-disable-line @typescript-eslint/camelcase
+    utm_term: 'term1234',          //eslint-disable-line @typescript-eslint/camelcase
+    utm_content: 'content1234',    //eslint-disable-line @typescript-eslint/camelcase
+  }
+
+  const params = Object.entries(utmFields).map(([key, val]) => `${key}=${val}`).join('&')
+
   render(
     // Using MemoryRouter is recommended way to test react-router-dom
     // https://reacttraining.com/react-router/web/guides/testing
-    <MemoryRouter initialEntries={['/org/default?utm_campaign=campaignid1234']}>
+    <MemoryRouter initialEntries={['/org/default?' + params]}>
       <Initialize/>
     </MemoryRouter>,
     { wrapper: UnstatedContainer } 
   )
 
   const userData = localStorage.getItem('voter-data')
-  expect(userData).toContain('campaignid1234')
+  for (const val of Object.values(utmFields)) {
+    expect(userData).toContain(val)
+  }
 })
