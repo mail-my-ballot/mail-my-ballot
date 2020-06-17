c8db473638a06606eeab48aef2ff224c0c540f95
Tianhui Michael Li <tianhuil@cs.princeton.edu>
2020-06-17 13:44:06 -0700

    Test for utm_campaign

--- packages/client/src/comp/Initialize.test.tsx
+++ packages/client/src/comp/Initialize.test.tsx
@@ -1,20 +1,19 @@
 import React from 'react'
 import { render } from '@testing-library/react'
+import { MemoryRouter } from 'react-router-dom'
 import { Initialize } from './Initialize'
 import { UnstatedContainer } from './StateContainer'
 
 test('Initialize picks up parameters', () => {
-  Object.defineProperty(window, 'location', {
-    value: {
-      href: 'https://example.com'
-    }
-  })
-
   render(
-    <Initialize/>,
+    // Using MemoryRouter is recommended way to test react-router-dom
+    // https://reacttraining.com/react-router/web/guides/testing
+    <MemoryRouter initialEntries={['/org/default?utm_campaign=campaignid1234']}>
+      <Initialize/>
+    </MemoryRouter>,
     { wrapper: UnstatedContainer } 
   )
 
   const userData = localStorage.getItem('voter-data')
-  expect(userData).toContain('uid')
+  expect(userData).toContain('campaignid1234')
 })
