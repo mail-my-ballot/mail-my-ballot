38fca59a1625eb4ff87a155f394d07bbb8ab5355
Tianhui Michael Li <tianhuil@cs.princeton.edu>
2020-06-17 17:07:38 -0700

    Better reporting in gm.proto.ts

--- packages/server/src/service/gm.proto.ts
+++ packages/server/src/service/gm.proto.ts
@@ -3,19 +3,22 @@ import { toContact } from './contact'
 import { Locale, toContactMethod } from '../common'
 
 const main = async() => {
-  const geoResult = await cacheGeocode('3600 Telegraph Rd, Bloomfield Twp, MI 48302')
+  const geoResult = await cacheGeocode('954 Baldwin St, Traverse City, MI 49686')
   if (!geoResult) return 
   
   console.log(JSON.stringify(geoResult, null, 2))
   const address = await toAddress(geoResult)
+  console.log('##### Address')
   console.log(address)
 
   if (!address) return
   const contact = await toContact(address as Locale)
+  console.log('##### Contact')
   console.log(contact)
 
   if (!contact) return
   const method = await toContactMethod(contact)
+  console.log('##### Method')
   console.log(method)
 }
 
