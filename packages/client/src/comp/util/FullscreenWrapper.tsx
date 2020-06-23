8fec71f55ba04781a3fe3ee4f7168338e9f23769
Lukas Danin <lukas.danin@gmail.com>
2020-06-23 17:26:35 -0300

    Implements FullscreenWrapper

---
+++ packages/client/src/comp/util/FullscreenWrapper.tsx
@@ -0,0 +1,69 @@
+import styled from 'styled-components'
+
+interface Props {
+  /**
+   * Centers the content of this Wrapper, vertically and horizontally.
+   *
+   * By default is true
+   */
+  centerContent?: boolean
+  /**
+   * Centers the content of immediate divs (e.g. `& > div`) of this Wrapper
+   *
+   * By default is false
+   */
+  centerChildContent?: boolean
+  /**
+   * Lays the content of immediate divs (e.g. `& > div`) of this Wrapper
+   * in Flex-columns
+   *
+   * By default is false
+   */
+  columnChildContent?: boolean
+}
+
+export const FullscreenWrapper = styled.div<Props>`
+  width: 100%;
+  min-height: 100vh;
+  padding: 4em 0;
+  box-sizing: border-box;
+  position: relative;
+  z-index: 9;
+  box-shadow: 0 0 14px #0003;
+
+  display: flex;
+  align-items: ${
+    p => (p.centerContent ?? true)
+      ? 'center'
+      : 'flex-start'
+  };
+  justify-content: ${
+    p => (p.centerContent ?? true)
+      ? 'center'
+      : 'flex-start'
+  };
+
+  & > div {
+    display: flex;
+    text-align: ${
+      p => p.centerChildContent
+        ? 'center'
+        : 'left'
+    };
+    flex-direction: ${
+      p => p.columnChildContent
+        ? 'column'
+        : 'row'
+    };
+    align-items: ${
+      p => p.centerChildContent
+        ? 'center'
+        : 'flex-start'
+    };
+    justify-content: ${
+      p => p.centerChildContent
+        ? 'center'
+        : 'flex-start'
+    };
+  }
+`
