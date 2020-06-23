daaca9841352421cd0b584037bb6aba3eec2ea26
Lukas Danin <lukas.danin@gmail.com>
2020-06-23 17:26:49 -0300

    Redesigns Blurb

--- packages/client/src/comp/Blurb.tsx
+++ packages/client/src/comp/Blurb.tsx
@@ -1,141 +1,71 @@
 import React from 'react'
 import styled from 'styled-components'
-import { toast } from 'react-toastify'
-import { RoundedButton } from './util/Button'
-import { StyleContainer } from './util/Container'
-import { useAppHistory } from '../lib/path'
-import { client } from '../lib/trpc'
-import { AddressContainer, FetchingDataContainer } from '../lib/unstated'
-import { AppForm } from './util/Form'
+import { Container } from 'muicss/react'
+import { cssQuery } from './util/cssQuery'
 
+import img1k from './img/blurb_bg_max_width_1k.jpg'
+import img2k from './img/blurb_bg_max_width_2k.jpg'
+import img6k from './img/blurb_bg_max_width_6k.jpg'
+import { EnterZip } from './util/EnterZip'
+import { FullscreenWrapper } from './util/FullscreenWrapper'
 
-const Background = styled.div`
-  top: 0;
-  left: 0;
-  min-width: 100%;
-  background: rgb(144,202,249);
-  background: linear-gradient(-45deg, rgba(144,202,249,1) 0%, rgba(30,136,229,1) 25%, rgba(21,101,192,1) 100%);
-  color: #f1f1ff;
-`
+const bgGradient = 'linear-gradient(to bottom right, #fffd, #fffa)'
 
-const Title = styled.h1`
-  font-weight: 100;
-  padding-top: 24px;
-  @media only screen and (max-width: 544px) {
-    padding-top: 8px;
+const Wrapper = styled(FullscreenWrapper)`
+  position: relative;
+  z-index: 9;
+  box-shadow: 0 0 14px #0003;
+
+  /*
+    Loads the background image according to the device's screen dimensions
+    We use manual queries here since these are mostly based on the dimensions
+    of the pictures used as background.
+  */
+  @media screen and (max-width: 640px) {
+    background-image: ${bgGradient}, url(${img1k});
+    background-size: cover;
+    background-position-x: 64%;
+    background-position-y: 0;
+  }
+  @media screen and (min-width: 641px) {
+    background-image: ${bgGradient}, url(${img1k});
+    background-size: cover;
+    background-position-x: 44%;
+    background-position-y: 0;
+  }
+  @media screen and (min-width: 1281px) {
+    background-image: ${bgGradient}, url(${img2k});
+    background-size: 115vw;
+    background-position-x: 74%;
+    background-position-y: 64%;
+  }
+  @media screen and (min-width: 1920px) {
+    background-image: ${bgGradient}, url(${img6k});
+    background-size: 100vw;
+  }
+
+  /* Device Adjustments */
+  ${cssQuery.desktop.all} {
+    h3 { width: 55%; }
+  }
+  ${cssQuery.mobile.landscape.all} {
+    h3 { width: 70%; }
   }
 `
 
-const Text = styled.p`
-  margin-bottom: 16px;
-`
-
-const FlexBox = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: flex-start;
-`
-
-const FlexContainer = styled.div`
-  margin-top: 24px;
-  display: flex;
-  flex-direction: row;
-  justify-contnet: flex-start;
-  align-content: center;
-`
-
-const ZipInput = styled.input`
-  padding: 14px 16px;
-  border: none;
-  box-shadow: none;
-  outline: none;
-  width: 84px;
-  border-radius: 4px;
-  margin-right: 1rem;
-`
-
-const SubmitButton = styled(RoundedButton)`
-  z-index: 0;
-  background: #4DB6AC;
-  color: #f1f1ff;
-  margin 0;
-  :hover {
-    background: #5DC6BC;
-    color: #f1f1ff;
-  }
-`
-
-export const Blurb: React.FC<{}> = () => {
-  const { path, pushAddress } = useAppHistory()
-  const { address } = AddressContainer.useContainer()
-  const zipRef = React.useRef<HTMLInputElement>(null)
-  const { fetchingData, setFetchingData } = FetchingDataContainer.useContainer()
-
-  // mobile browsers don't support 100vh, so use this trick instead
-  // https://chanind.github.io/javascript/2019/09/28/avoid-100vh-on-mobile-web.html
-  // Also, need to use a state variable instead of a simpler ts variable
-  // https://stackoverflow.com/a/56156394/8930600
-  const [height, setHeight] = React.useState('100vh')
-  React.useEffect(() => {
-    setHeight(`${window.innerHeight}px`)
-  }, [])
-
-  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
-    event.persist()  // allow async function call
-    event.preventDefault()
-    const zip = zipRef?.current?.value
-    if (!zip) return
-    setFetchingData(true)
-    const resp = await client.fetchState(zip)
-    if (resp.type === 'error') {
-      toast.error('Error finding the ZIP code')
-    } else {
-      pushAddress(resp.data, zip)
-    }
-    setFetchingData(false)
-  }
-
-  const defaultValue = () => {
-    if (path?.type === 'address' && path.zip) {
-      return path.zip
-    } else {
-      return address?.postcode ?? undefined
-    }
-  }
-
-  return <Background style={{height}}>
-    <StyleContainer>
-      <FlexBox style={{height}}>
-        <Title>Vote by Mail</Title>
-        <Text>
-          Voting by mail is a secure, time-tested, and easy way to vote.  Your ballot arrives safely in the mail weeks before the election and can be filled out and returned at your convenience.
-        </Text>
-        <Text>Sign up today in <b>2 minutes</b> before your state deadline expires.
-        </Text>
-        <AppForm onSubmit={handleSubmit}>
-          <Text><b>Enter your ZIP code</b> to get started</Text>
-          <FlexContainer>
-            {/* id is used by WarningMsg to fill out zipcode */}
-            <ZipInput
-              id='start-zip'
-              data-testid='start-zip'
-              type='text'
-              pattern='[0-9]{5}'
-              placeholder='ZIP code'
-              defaultValue={defaultValue()}
-              ref={zipRef} />
-            <SubmitButton
-              id='start-submit'
-              data-testid='start-submit'
-              variant='raised'
-              disabled={fetchingData}
-            >
-              Start
-            </SubmitButton>
-          </FlexContainer>
-        </AppForm>
-      </FlexBox>
-    </StyleContainer>
-  </Background>
+export const Blurb: React.FC = () => {
+  // We don't directly return a Container here since setting the BG feels
+  // hacky (if we use Fluid Containers we'd need to tweak the max-width/padding
+  // manually).
+  return <Wrapper columnChildContent={true} centerChildContent={true}>
+    <Container>
+      <h3>
+        MailMyBallot streamlines state vote-by-mail applications by digitizing the voter’s signup process.
+      </h3>
+      <h5 style={{ fontWeight: 'normal', margin: '2em 0' }}>
+        Enter your ZIP Code to get started
+      </h5>
+      <EnterZip/>
+    </Container>
+  </Wrapper>
 }
