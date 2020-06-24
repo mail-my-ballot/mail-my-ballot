ad43a334cbfdcfe814f8e3a1421daf799974ea70
Lukas Danin <lukas.danin@gmail.com>
2020-06-24 00:18:09 -0300

    Compress images bigger than 1mb

--- packages/client/src/comp/util/Upload.tsx
+++ packages/client/src/comp/util/Upload.tsx
@@ -7,13 +7,20 @@ import { Muted } from './Text'
 const toDataUrl = (file: File): Promise<string> => {
   return new Promise((resolve, reject) => {
     const reader = new FileReader()
-    reader.readAsDataURL(file)  
+    reader.readAsDataURL(file)
     reader.onload = () => { resolve(reader.result as string) }
-    reader.onerror = () => { 
+    reader.onerror = () => {
       reader.abort()
       reject(reader.result)
     }
   })
+}
+
+const getBase64Size = (imgSrc: string) => {
+  const split = imgSrc.split(',')
+  // To prevent errors
+  if (split.length === 0) return 0
+  return split[split.length-1].length * 0.75
 }
 
 interface Props {
@@ -23,7 +30,7 @@ interface Props {
   maxSizeMB?: number
 }
 
-interface Image {
+interface ImageDetails {
   name: string
   data: string
 }
@@ -43,11 +50,78 @@ export const Upload: React.FC<Props> = ({
   maxSizeMB,
 }) => {
   const ref = React.useRef<HTMLInputElement | null>()
-  const [image, setImage] = React.useState<Image>()
+  const [image, setImage] = React.useState<ImageDetails>()
+  // If the image is above 1mb it is loaded and compressed used this canvas
+  const canvasRef = React.useRef<HTMLCanvasElement | null>()
+  const anchorRef = React.useRef<HTMLAnchorElement | null>()
 
   const onClick = (event: React.MouseEvent<HTMLElement, MouseEvent>) => {
     event.preventDefault()
     if (ref.current) ref.current.click()
+  }
+
+  // The highest value, in pixels, that either width or height can have
+  const highestDimension = 2000
+  const Compress = async (file: File) => {
+    const image = new Image()
+    const data = await toDataUrl(file)
+    image.src = data
+
+    // It takes some time to load the image, which is why we have to await
+    // for the image to load with this function
+    image.onload = () => {
+      const context = canvasRef.current?.getContext('2d')
+
+      // Clears previous uploads & resets canvas size
+      if (context) {
+        context.canvas.width = highestDimension
+        context.canvas.height = highestDimension
+      }
+      context?.clearRect(0, 0, context?.canvas.width, context?.canvas.height)
+
+      // Decide if the image needs resizing
+      const { naturalWidth: width, naturalHeight: height } = image
+      let newWidth = width
+      let newHeight = height
+
+      if (Math.max(width, height) > highestDimension) {
+        const portrait = height > width
+        // Downscale ratio
+        const ratio = portrait ? width / height : height / width
+        if (portrait) {
+          newHeight = highestDimension
+          newWidth = highestDimension * ratio
+        } else {
+          newWidth = highestDimension
+          newHeight = highestDimension * ratio
+        }
+      }
+
+      // The canvas might have some unused space since (unless the picture is a square)
+      // either the width or height are going to be 2000px. We don't need
+      // to save that.
+      if (context) {
+        context.canvas.width = newWidth
+        context.canvas.height = newHeight
+      }
+
+      context?.drawImage(image, 0, 0, newWidth, newHeight)
+      const resized = context?.canvas.toDataURL('image/jpeg') ?? ''
+
+      if (process.env.REACT_APP_ENVIRONMENT !== 'production') {
+        // Updates the anchor element so we might download the image and
+        // compare its compressed size, an estimate of the size is sent
+        // to the console
+        if (anchorRef.current) {
+          console.log(`Image was resized to ~= ${getBase64Size(resized)} bytes`)
+          anchorRef.current.href = resized
+          anchorRef.current.style.display = 'inline-block'
+        }
+      }
+
+      setImage({name: file.name, data: resized})
+      setDataString(resized)
+    }
   }
 
   const maxSizeMBReal = maxSizeMB ?? 1
@@ -56,7 +130,7 @@ export const Upload: React.FC<Props> = ({
     if (event.target.files && event.target.files.length > 0) {
       const file = event.target.files[0]
       if (file.size > (maxSizeMBReal * 1024 * 1024)) {
-        window.alert(`File size is limited to ${maxSizeMBReal}MB`)
+        await Compress(file)
       } else {
         const data = await toDataUrl(file)
         setImage({name: file.name, data: data})
@@ -87,7 +161,7 @@ export const Upload: React.FC<Props> = ({
                 }<h1 style={{marginTop: '0', paddingTop: '0'}}><i className="fa fa-upload" aria-hidden="true"/></h1>
                   <Muted>Limit: {maxSizeMBReal}MB</Muted>
                   <SmallButton color='primary' style={centerBlock} >{label}</SmallButton>
-                </>   
+                </>
           }
           </FlexBox>
         </div>)
@@ -104,5 +178,31 @@ export const Upload: React.FC<Props> = ({
       accept='image/*,.pdf'
       required={required}
     />
+    <canvas
+      ref={el => canvasRef.current = el}
+      width={highestDimension}
+      height={highestDimension}
+      style={{ display: 'none' }}
+    />
+    {
+      // Allows to download the image when on debug versions
+      process.env.REACT_APP_ENVIRONMENT !== 'production' &&
+      <a
+        href='#downloadImage'
+        download='resized.jpg'
+        ref={el => anchorRef.current = el}
+        style={{ display: 'none' }}
+      >
+        Download Resized
+        {` ${(getBase64Size(image?.data ?? '') / Math.pow(1024, 2)).toFixed(2)}`} MB
+        <button onClick={() => {
+          if (anchorRef.current) {
+            anchorRef.current.style.display = 'none'
+          }
+        }}>
+          Hide This
+        </button>
+      </a>
+    }
   </div>
 }
