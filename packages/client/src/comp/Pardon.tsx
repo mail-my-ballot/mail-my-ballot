c53ae5d913e793c2b87d1f6fa797fd5f8c12585d
Tianhui Michael Li <tianhuil@cs.princeton.edu>
2020-07-01 17:44:26 -0700

    Update Pardon with language

--- packages/client/src/comp/Pardon.tsx
+++ packages/client/src/comp/Pardon.tsx
@@ -4,9 +4,6 @@ import Container from 'muicss/lib/react/container'
 export const Pardon = () => (<Container>
   <h1>Pardon Our Apperance</h1>
   <p>
-    As you likely already know, <a href='https://www.voteathome.org/'>National Vote at Home Institute</a> (NVAHI) is the official home of MailMyBallot.org. NVAHI’s top priority with MailMyBallot.org remains offering a tool integration for mail ballot requests to all secure partners with the mission of making sure every voter can participate in a safe, equitable, accessible election this fall. NVAHI is in the process of deciding how the project aligns with the organization’s core mandate as the app grows, in anticipation for the November election.
-  </p>
-  <p>
-  In the meantime, the voter-facing portion of the MailMyBallot.org site will be paused effective immediately. You will still be able to access your data from the organizer dashboard. The NVAHI and MailMyBallot.org teams are dedicated to offering you the best tool possible going forward. Thank you for your help in building up the work thus far; we will let you know immediately once the MailMyBallot.org growth plan has been solidified, likely in the next 2 weeks. We are sorry for any inconvenience this may cause.
+  This site is temporarily down and we are working to get it back in service as quickly as possible.
   </p>
 </Container>)
